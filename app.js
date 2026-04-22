fd5afdb4843b1aa8766ad334a91a711f2c103143
sumia34 <ahsn987234@gmail.com>
2026-04-22 10:07:13 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 const express = require("express");
 const path = require("path");
-const connectDB=require("./config/db.js")
-connectDB();
+const connectDB = require("./config/db.js");
+
 const app = express();
-//set ejs as a template engine
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
-app.use(express.urlencoded({entended:true}))
+
+// Connect Database
+connectDB();
+
+// Set EJS
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get('/file',(req,res)=>{
-  res.render("index")
-})
 
+// Test Route
+app.get("/file", (req, res) => {
+res.render("index");
+});
+
+// Routes
 const userRoutes = require("./routes/routes");
-
 app.use("/users", userRoutes);
 
-app.listen(5000, () => {
+// Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
 console.log("Server running");
 });
