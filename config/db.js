9d76281ad475795d1f416597631247e9b0515df0
sumia34 <ahsn987234@gmail.com>
2026-04-22 10:18:44 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (err) {
     console.log("DB Connection Error:", err);
